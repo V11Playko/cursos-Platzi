471a515fe7d5b8a4bc58f7df7845fa78c8d14cb9
Heinner Vega <heinnervega20@gmail.com>
2022-01-25 13:23:03 -0500

    <Check>Conexión a Mongo

--- plazi-Ride/backend/config/datastores.js
+++ plazi-Ride/backend/config/datastores.js
@@ -50,6 +50,10 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mongo',
+    port: '27017',
+    host: '127.0.0.1',
+    database: 'platzi-ride',
 
   },
 
