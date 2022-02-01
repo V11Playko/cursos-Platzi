85638bde9231a167cdf0e5edd41d26be074f5e6c
Heinner Vega <heinnervega20@gmail.com>
2022-02-01 12:00:29 -0500

    Validando la información - Implementando Joi

--- platzi-Overflow/routes.js
+++ platzi-Overflow/routes.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Joi = require("joi");
 const site = require("./controllers/site");
 const user = require("./controllers/user");
 
@@ -16,6 +17,15 @@ module.exports = [
   },
   {
     method: "POST",
+    options: {
+      validate: {
+        payload: {
+          name: Joi.string().required().min(3),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6),
+        },
+      },
+    },
     path: "/create-user",
     handler: user.createUser,
   },
