f7bdfce074bfba127adce09950f6dcea4141edaf
Heinner Vega <heinnervega20@gmail.com>
2022-02-02 17:47:36 -0500

    Creación de plugins - Implementando un API REST

---
+++ platzi-Overflow/lib/api.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const Boom = require("boom");
+const Joi = require("joi");
+const questions = require("../models/index").questions;
+
+module.exports = {
+  name: "api-rest",
+  version: "1.0.0",
+  async register(server, options) {
+    const prefix = options.prefix || "api";
+
+    server.route({
+      method: "GET",
+      path: `/${prefix}/question/{key}`,
+      options: {
+        validate: {
+          params: {
+            key: Joi.string().required(),
+          },
+          failAction: failValidation,
+        },
+      },
+      handler: async (req, h) => {
+        let result;
+        try {
+          result = await questions.getOne(req.params.key);
+          if (!result) {
+            return Boom.notFound(
+              `No se pudo encontrar la pregunta ${req.params.key}`
+            );
+          }
+        } catch (error) {
+          return Boom.badImplementation(
+            `Hubo un error buscando ${req.params.key} - ${error}`
+          );
+        }
+
+        return result;
+      },
+    });
+
+    server.route({
+      method: "GET",
+      path: `/${prefix}/questions/{amount}`,
+      options: {
+        validate: {
+          params: {
+            amount: Joi.number().integer().min(1).max(20).required(),
+          },
+          failAction: failValidation,
+        },
+      },
+      handler: async (req, h) => {
+        let result;
+        try {
+          result = await questions.getLast(req.params.amount);
+          if (!result) {
+            return Boom.notFound(`No se pudo recuperar las preguntas`);
+          }
+        } catch (error) {
+          return Boom.badImplementation(
+            `Hubo un error buscando las preguntas - ${error}`
+          );
+        }
+
+        return result;
+      },
+    });
+
+    function failValidation(req, h, err) {
+      return Boom.badRequest("Por favor use los parámetros correctos");
+    }
+  },
+};
