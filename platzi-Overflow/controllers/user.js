fd7ef2ed534783941bcf4be9ff04c94c18844667
Heinner Vega <heinnervega20@gmail.com>
2022-02-02 07:38:40 -0500

    Visualización de errores

--- platzi-Overflow/controllers/user.js
+++ platzi-Overflow/controllers/user.js
@@ -1,49 +1,63 @@
-"use strict";
+'use strict'
 
-const Boom = require("boom");
-const users = require("../models/index").users;
+const Boom = require('boom')
+const users = require('../models/index').users
 
-async function createUser(req, h) {
-  let result;
+async function createUser (req, h) {
+  let result
   try {
-    result = await users.create(req.payload);
+    result = await users.create(req.payload)
+    console.log(`Usuario registrado ${result}`)
   } catch (error) {
-    console.error(error);
-    return h.response("Problemas creando el usuario").code(500);
+    console.error(error)
+    return h.view('register', {
+      title: 'Registro',
+      error: 'Error creando el usuario'
+    })
   }
 
-  return h.response(`Usuario creado ID: ${result}`);
+  return h.view('register', {
+    title: 'Registro',
+    success: 'Usuario creado exitosamente'
+  })
 }
 
-function logout(req, h) {
-  return h.redirect("/login").unstate("user");
+function logout (req, h) {
+  return h.redirect('/login').unstate('user')
 }
 
-async function validateUser(req, h) {
-  let result;
+async function validateUser (req, h) {
+  let result
   try {
-    result = await users.validateUser(req.payload);
+    result = await users.validateUser(req.payload)
     if (!result) {
-      return h.response("Email y/o contraseña incorrecta").code(401);
+      return h.view('login', {
+        title: 'Login',
+        error: 'Email y/o contraseña incorrecta'
+      })
     }
   } catch (error) {
-    console.error(error);
-    return h.response("Problemas validando el usuario").code(500);
+    console.error(error)
+    return h.view('login', {
+      title: 'Login',
+      error: 'Problemas validando el usuario'
+    })
   }
 
-  return h.redirect("/").state("user", {
+  return h.redirect('/').state('user', {
     name: result.name,
-    email: result.email,
-  });
+    email: result.email
+  })
 }
 
-function failValidation(req, h, err) {
-  return Boom.badRequest("Falló la validación", req.payload);
+function failValidation (req, h, err) {
+  return Boom.badRequest('Falló la validación', req.payload)
 }
 
 module.exports = {
   createUser: createUser,
   failValidation: failValidation,
   logout: logout,
-  validateUser: validateUser,
-};
+  validateUser: validateUser
+}
+
--- platzi-Overflow/routes.js
+++ platzi-Overflow/routes.js
@@ -56,12 +56,17 @@ module.exports = [
   },
   {
     method: "GET",
-    path: "/{param*}",
+    path: "/assets/{param*}",
     handler: {
       directory: {
         path: ".",
         index: ["index.html"],
       },
     },
+  },
+  {
+    method: ["GET", "POST"],
+    path: "/{any*}",
+    handler: site.notFound,
   },
 ];
