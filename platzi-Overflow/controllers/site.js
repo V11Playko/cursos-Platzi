e75864dad5f91d4587138f9a30c1332ec37d2d42
Heinner Vega <heinnervega20@gmail.com>
2022-02-02 07:53:02 -0500

    Controlar el error 404 en inert y el error de validación

--- platzi-Overflow/controllers/site.js
+++ platzi-Overflow/controllers/site.js
@@ -33,8 +33,18 @@ function notFound(req, h) {
   return h.view("404", {}, { layout: "error-layout" }).code(404);
 }
 
+function fileNotFound(req, h) {
+  const response = req.response;
+  if (response.isBoom && response.output.statusCode === 404) {
+    return h.view("404", {}, { layout: "error-layout" }).code(404);
+  }
+
+  return h.continue;
+}
+
 module.exports = {
   home: home,
+  fileNotFound: fileNotFound,
   login: login,
   notFound: notFound,
   register: register,
--- platzi-Overflow/controllers/user.js
+++ platzi-Overflow/controllers/user.js
@@ -1,63 +1,72 @@
-'use strict'
+"use strict";
 
-const Boom = require('boom')
-const users = require('../models/index').users
+const users = require("../models/index").users;
 
-async function createUser (req, h) {
-  let result
+async function createUser(req, h) {
+  let result;
   try {
-    result = await users.create(req.payload)
-    console.log(`Usuario registrado ${result}`)
+    result = await users.create(req.payload);
+    console.log(`Usuario registrado ${result}`);
   } catch (error) {
-    console.error(error)
-    return h.view('register', {
-      title: 'Registro',
-      error: 'Error creando el usuario'
-    })
+    console.error(error);
+    return h.view("register", {
+      title: "Registro",
+      error: "Error creando el usuario",
+    });
   }
 
-  return h.view('register', {
-    title: 'Registro',
-    success: 'Usuario creado exitosamente'
-  })
+  return h.view("register", {
+    title: "Registro",
+    success: "Usuario creado exitosamente",
+  });
 }
 
-function logout (req, h) {
-  return h.redirect('/login').unstate('user')
+function logout(req, h) {
+  return h.redirect("/login").unstate("user");
 }
 
-async function validateUser (req, h) {
-  let result
+async function validateUser(req, h) {
+  let result;
   try {
-    result = await users.validateUser(req.payload)
+    result = await users.validateUser(req.payload);
     if (!result) {
-      return h.view('login', {
-        title: 'Login',
-        error: 'Email y/o contraseña incorrecta'
-      })
+      return h.view("login", {
+        title: "Login",
+        error: "Email y/o contraseña incorrecta",
+      });
     }
   } catch (error) {
-    console.error(error)
-    return h.view('login', {
-      title: 'Login',
-      error: 'Problemas validando el usuario'
-    })
+    console.error(error);
+    return h.view("login", {
+      title: "Login",
+      error: "Problemas validando el usuario",
+    });
   }
 
-  return h.redirect('/').state('user', {
+  return h.redirect("/").state("user", {
     name: result.name,
-    email: result.email
-  })
+    email: result.email,
+  });
 }
 
-function failValidation (req, h, err) {
-  return Boom.badRequest('Falló la validación', req.payload)
+function failValidation(req, h, err) {
+  const templates = {
+    "/create-user": "register",
+    "/validate-user": "login",
+  };
+
+  return h
+    .view(templates[req.path], {
+      title: "Error de validación",
+      error: "Por favor complete los campos requeridos",
+    })
+    .code(400)
+    .takeover();
 }
 
 module.exports = {
   createUser: createUser,
   failValidation: failValidation,
   logout: logout,
-  validateUser: validateUser
-}
-
+  validateUser: validateUser,
+};
