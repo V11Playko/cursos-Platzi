e39d93b4eb8107cb0ad6be8b2acbce5a602e6ead
V11Playko <heinnervega20@gmail.com>
2022-07-10 15:15:37 -0500

    Pruebas

--- Curso programacion orientada a objetos POO/java/UberBlack.java
+++ Curso programacion orientada a objetos POO/java/UberBlack.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 import java.util.Map;
 
 class UberBlack extends Car {
-    Map<String, ArrayList<String,Integer>> typeCarAccepted;
+    Map<String, Map<String, Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
-    public UberBlack(String license, Account driver, 
-    Map<String, ArrayList<String,Integer>> typeCarAccepted,
-    ArrayList<String> seatsMaterial){
+    public UberBlack(String license, Account driver,
+            Map<String, Map<String, Integer>> typeCarAccepted,
+            ArrayList<String> seatsMaterial) {
         super(license, driver);
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
