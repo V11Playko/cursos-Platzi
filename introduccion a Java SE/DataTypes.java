29935937ff68440c4d0c2c11dc5bbd6484f7eadd
V11Playko <heinnervega20@gmail.com>
2022-07-11 16:07:10 -0500

    Tipos de datos numericos

---
+++ introduccion a Java SE/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        // Numero entero (max. 10 numeros)
+        int n = 1234567890;
+
+        //Para diferenciarlo de Int colocar una L al final
+        long nL = 12345678901L;
+
+        //Numero grande en decimales
+        double nD = 123.456;
+
+        //Para diferenciarlo de Double colocar una F al final
+        float nF = 123.456F;
+
+    }
+}
