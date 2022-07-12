7172d526d82299424f70b7ee826d3411ef213e8d
V11Playko <heinnervega20@gmail.com>
2022-07-12 16:28:59 -0500

    Operadores logicos y expresiones booleanas

---
+++ introduccion a Java SE/src/LogicOperations.java
@@ -0,0 +1,22 @@
+public class LogicOperations {
+
+    public static void main(String[] args) {
+
+        boolean condicionA = true; // verdadero
+        boolean condicionB = false; // falso
+
+        boolean condicionC = 2 + 2 == 4; // verdadero
+        boolean condicionD = 2 + 2 == 5; // falso
+
+        boolean condicionE = "Pepito" == "Pepito"; // verdadero
+        boolean condicionF = "Pepito" == "Pepe"; // falso
+
+        System.out.println(condicionA);
+        System.out.println(condicionB);
+        System.out.println(condicionC);
+        System.out.println(condicionD);
+        System.out.println(condicionE);
+        System.out.println(condicionF);
+
+    }
+}
