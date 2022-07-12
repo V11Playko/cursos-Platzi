56b3138d14e3a699c43a731196aece1d3457e7d6
V11Playko <heinnervega20@gmail.com>
2022-07-12 12:34:48 -0500

    Haciendo archivos jar y resolviendo 1 ejercicio

---
+++ introduccion a Java SE/src/Family.java
@@ -0,0 +1,23 @@
+public class Family {
+
+    public static void main(String[] args) {
+
+        String my_Name = "Heinner";
+        String mom_Name = "Yolanda";
+        String father_Name = "Gonzalo";
+        String bro_Name = "Brayan";
+
+        int myAge = 19;
+        int momAge = 51;
+        int fatherAge = 63;
+        int brotherAge = 23;
+
+        // Aqui imprimo los nombres de mi familia
+        System.out.println("Yo " + my_Name + " presento a mi familia, mi mamá " + mom_Name + ", mi papá " + father_Name + " y mi hermano " + bro_Name + " .");
+
+        // Aqui imprimo las edades respectivamente
+        System.out.println("Esta son nuestras edades respectivamente:");
+        System.out.println(myAge + ", " + momAge + ", " + fatherAge + ", " + brotherAge + ".");
+
+    }
+}
