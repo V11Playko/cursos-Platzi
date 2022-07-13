1db0f54269077e68667484da83080a4d24a7bb1d
V11Playko <heinnervega20@gmail.com>
2022-07-12 21:19:33 -0500

    Implementando funciones

--- introduccion a Java SE/src/Functions.java
+++ introduccion a Java SE/src/Functions.java
@@ -2,7 +2,61 @@ public class Functions {
 
     public static void main(String[] args) {
         double y = 3;
+        double area = circleArea(y);
+        double quality = 500;
+        System.out.println(area);
 
-        
+        //Area de un circulo
+        //pi * r
+        System.out.println("Area Circulo");
+        System.out.println(area);
+
+        //Area de una esfera
+        //4*PI*r2
+        double areaSphere = sphereArea(y);
+        System.out.println("Area Sphere");
+        System.out.println(areaSphere);
+
+        //Volumen de una esfera
+        //(4/3)*pi * r3
+        double volumenSphere = sphereVolumen(y);
+        System.out.println("Volumen sphere");
+        System.out.println(volumenSphere);
+
+        System.out.println("PESOS A DOLARES " + convertToDollar(quality, "MXN") + " MXN");
+        System.out.println("PESOS A DOLARES " + convertToDollar(quality, "COP") + " COP");
     }
+
+    public static double circleArea(double y) {
+        return Math.PI * Math.pow(y,2);
+    }
+
+    public static double sphereArea (double y)
+    {
+        return Math.PI * 4 * Math.pow(y,2);
+    }
+    public static double sphereVolumen (double y)
+    {
+        return   Math.PI * (4/3) * Math.pow(y,3);
+    }
+
+    //Funciones B
+    public static double convertToDollar (double quantity, String currency)
+    {
+        //MXN COP
+        switch (currency) {
+            case "MXN":
+                quantity = quantity * 0.052;
+                break;
+            case "COP":
+                quantity = quantity * 0.00031;
+                break;
+
+        }
+        return quantity;
+    }
+
+
+
+
 }
