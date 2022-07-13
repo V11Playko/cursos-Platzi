32a631ee2380f8b7da996b2b86a4fc4689060b65
V11Playko <heinnervega20@gmail.com>
2022-07-13 08:44:58 -0500

    Bucle do while

---
+++ introduccion a Java SE/src/DoWhileLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int response;
+
+        do {
+            System.out.println("Selecciona el número de la opción deseada");
+            System.out.println("1. Movies");
+            System.out.println("2. Series");
+            System.out.println("0. Salir");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.parseInt(sc.nextLine());
+
+            switch (response) {
+                case 0 -> System.out.println("Gracias por visitarnos");
+                case 1 -> System.out.println("Movies");
+                case 2 -> System.out.println("Series");
+                default -> System.out.println("Selecciona una opción correcta");
+            }
+        } while (response != 0);
+
+        System.out.println("Se terminó el programa");
+
+    }
+}
