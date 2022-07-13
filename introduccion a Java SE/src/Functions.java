f934ad5c70d6bf9db1ca4ea4f13329f770dcf0c5
V11Playko <heinnervega20@gmail.com>
2022-07-13 07:47:47 -0500

    Documentaciona funciones

--- introduccion a Java SE/src/Functions.java
+++ introduccion a Java SE/src/Functions.java
@@ -1,5 +1,11 @@
 public class Functions {
 
+    /**
+     * Descripcion: Esta es la funcion principal de este codigo
+     *
+     * <p>En ella se encuentran algunas variables y las salidas
+     *    por pantalla de todo el codigo</p>
+     * */
     public static void main(String[] args) {
         double y = 3;
         double area = circleArea(y);
@@ -25,21 +31,39 @@ public class Functions {
         System.out.println("PESOS A DOLARES " + convertToDollar(quality, "MXN") + " MXN");
         System.out.println("PESOS A DOLARES " + convertToDollar(quality, "COP") + " COP");
     }
-
+/**
+ * Descripcion de la funcion: Esta funcion saca el area de un circulo
+ * */
     public static double circleArea(double y) {
         return Math.PI * Math.pow(y,2);
     }
 
+    /**
+     *  Descripcion de la funcion: Esta funcion saca el area de una esfera
+     * */
     public static double sphereArea (double y)
     {
         return Math.PI * 4 * Math.pow(y,2);
     }
+
+    /**
+     * Descripcion de la funcion: Esta funcion saca el volumen de una esfera
+     * */
     public static double sphereVolumen (double y)
     {
         return   Math.PI * (4/3) * Math.pow(y,3);
     }
 
     //Funciones B
+
+    /**
+     * Descripción general de nuestra función.
+     *
+     * @param quantity Descripción del parámetro quanity.
+     * @param currency Descripción del parámetro currency (MXN o COP).
+     * @return Descripción del valor que devolvemos en esta función.
+     *
+     * */
     public static double convertToDollar (double quantity, String currency)
     {
         //MXN COP
