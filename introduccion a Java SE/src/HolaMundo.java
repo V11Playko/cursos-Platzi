b809392a3e1ed28acc91d7b6fc2e4f3257859b38
V11Playko <heinnervega20@gmail.com>
2022-07-15 21:04:57 -0500

    prueba

--- introduccion a Java SE/src/HolaMundo.java
+++ introduccion a Java SE/src/HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Siiiii hpta");
+        byte i = 1; byte j = 1; byte k = (byte) (i+j);
     }
 }
