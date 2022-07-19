aff027b9dd34a1dfc0c64927b75fafed923b0ae4
V11Playko <heinnervega20@gmail.com>
2022-07-19 12:08:20 -0500

    Creando elementos estaticos

--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
@@ -1,21 +1,26 @@
-import javax.print.Doc;
 
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
         System.out.println("El nombre del Doctor asignado es: " + name);
     }
 
-    // Comportamientos
+    //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 
 }
