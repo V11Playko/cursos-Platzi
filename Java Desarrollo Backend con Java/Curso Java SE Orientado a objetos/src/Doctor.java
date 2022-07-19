db3e7ffdf8aadc5173d8be466f4c16e7d7db0391
V11Playko <heinnervega20@gmail.com>
2022-07-19 09:17:13 -0500

    Aplicando metodo constructor

--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
@@ -1,7 +1,17 @@
+import javax.print.Doc;
+
 public class Doctor {
     int id;
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
 
     // Comportamientos
     public void showName(){
--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Heinner Vega");
         myDoctor.name = "Heinner Vega";
         myDoctor.showName();;
     }
