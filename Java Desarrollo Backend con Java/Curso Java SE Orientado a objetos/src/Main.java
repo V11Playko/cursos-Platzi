be00177167f0827dfb2ba61c10f3cd7c559c277f
V11Playko <heinnervega20@gmail.com>
2022-07-20 17:16:24 -0500

    Getters y Setter

--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
@@ -5,13 +5,40 @@ public class Main {
 
         //showMenu();
 
-        Doctor myDoctor = new Doctor("Heinner Vega", "Pediatria");
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
-        Patient patient = new Patient(myDoctor.name = "Heinner", myDoctor.email = "ajaja@gmail.com");
-        System.out.println(patient.email);
+        int i = 0;
+        int b = 2;
+        b = i;
+        //b=0
+        String name = "Ann";
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());*/
+
     }
 
-    
+
+
 }
