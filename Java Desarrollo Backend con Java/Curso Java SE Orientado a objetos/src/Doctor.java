b5e286a39bf2887ba6567429b55bdfff9908c575
V11Playko <heinnervega20@gmail.com>
2022-07-20 11:45:53 -0500

    Sobrecarga de metodos y constructores

--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
@@ -1,4 +1,3 @@
-
 public class Doctor {
     //Atributos
     static int id = 0; //Autoincrement
@@ -7,11 +6,14 @@ public class Doctor {
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
-        id++;
+
     }
 
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("El nombre del Doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
     }
 
     //Comportamientos
--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Main.java
@@ -1,11 +1,15 @@
-import static ui.UIMenu.MONTHS;
-import static ui.UIMenu.showMenu;
+import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
 
+        Doctor myDoctor = new Doctor("Heinner Vega", "Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
+
+
 
 }
