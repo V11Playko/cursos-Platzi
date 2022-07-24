3db3ce9bbb7337b3514052c9670a8601975fa73c
V11Playko <heinnervega20@gmail.com>
2022-07-24 12:57:55 -0500

    Ennumeratios, super y this

--- Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
+++ Java Desarrollo Backend con Java/Curso Java SE Orientado a objetos/src/Doctor.java
@@ -1,29 +1,69 @@
-public class Doctor {
-    //Atributos
-    static int id = 0; //Autoincrement
-    String name;
-    String email;
-    String speciality;
+import java.util.ArrayList;
+import java.util.Date;
 
-    Doctor(){
-        System.out.println("Construyendo el Objeto Doctor");
+public class Doctor extends User {
+    //Atributo
+    private String speciality;
 
-    }
-
-    Doctor(String name, String speciality){
+    Doctor(String name, String email){
+        super(name,email);
         System.out.println("El nombre del Doctor asignado es: " + name);
-        id++;
-        this.name = name;
         this.speciality = speciality;
     }
 
-    //Comportamientos
-    public void showName(){
-        System.out.println(name);
+    public String getSpeciality() {
+        return speciality;
     }
 
-    public void showId(){
-        System.out.println("ID Doctor: " + id);
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new Doctor.AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 
 }
