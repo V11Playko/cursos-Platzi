03f44e383aa3f54694303a51931f355ec734c0da
V11Playko <heinnervega20@gmail.com>
2022-08-01 17:17:15 -0500

    Estructura de control WHEN

--- Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Main.kt
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Main.kt
@@ -2,33 +2,45 @@
 const val PI = 3.1416
 
 fun main(args: Array<String>) {
-    val nombre = "Gera"  //Declaramos una variable y le asignamos un valor
-    //Aqui empieza nuestra estructura de control IF
-    //Tenemos una prueba logica (donde preguntamos si nombre no esta vacia)
-    //Si esto es verdad [IF] entonces vamos a imprimer la cantidad de caracteres que hay en la variable nombre
-    //Si esto no es verdad [else] vamos a imprimir un mensaje de error diciendo que la variable esta vacia
-    if (nombre.isNotEmpty()) {
-        println("El largo de nuestra variable nombre es ${nombre.length}")
-    } else {
-        println("Error, la variable se encuentra vacia")
+    val pais = "Argentina"
+
+    when(pais) {
+        "Argentina" -> {
+            println("El idioma de ${pais} es Español");
+        }
+
+        "Alemania" -> {
+            println("El idioma de ${pais} es Aleman");
+        }
+
+        "EEUU" -> {
+            println("El idioma de ${pais} es Ingles");
+        }
+
+        "Brasil" -> {
+            println("El idioma de ${pais} es Portugues");
+        }
+
+        else -> {
+            println("No conocemos el idioma");
+        }
     }
 
-    // Asignación de if
-    var mensaje : String
-    if (nombre.length > 8){
-        mensaje = "Tienes un nombre largo!"
-    } else {
-        mensaje = "Tienes un nombre corto"
+    val edad = 33;
+
+    when(edad) {
+        in 0..3 -> {
+            println("Tu edad es ${edad} eres un bebe");
+        } in 4..11 -> {
+        println("Tu edad es ${edad} eres un niño");
+    } in 12..18 -> {
+        println("Tu edad es ${edad} eres un adolecente");
+    } in 19..59 -> {
+        println("Tu edad es ${edad} eres un adulto");
+    } in 60..99 -> {
+        println("Tu edad es ${edad} eres un adulto mayor");
+    } else -> {
+        println("😲");
     }
-    println(mensaje)
-
-    // Asignación de if inmutable
-    val mensajeInmutable = if (nombre.length > 8) "Tienes un nombre largo!" else "Tienes un nombre corto"
-    println(mensajeInmutable)
-
-    // Asignación de variable inmutable con else if
-    val mensajeInmutable2= if (nombre.length > 8) "Tienes un nombre largo!" else if(nombre.isEmpty()) "Este nombre esta vacio :(" else "Tienes un nombre corto"
-    println(mensajeInmutable2)
-
-
+    }
 }
