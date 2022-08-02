a17ebb3694c7087fa183636114f207b08beb601e
V11Playko <heinnervega20@gmail.com>
2022-08-02 16:24:24 -0500

    Funciones y funciones de extensión

---
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/FunctionsAndFunctionsExtension.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val letraAleatoria = "En Platzi nunca paramos de aprender".randomCase()
+    println(letraAleatoria)
+}
+
+private fun String.randomCase(): String {
+    val numeroAleatorio = 0..99
+    val resultadoAleatorio = numeroAleatorio.random()
+    return if (resultadoAleatorio.rem(2) == 0) toUpperCase() else toLowerCase()
+}
