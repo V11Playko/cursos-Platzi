a9c026d3dad2cf642bb973b50acbcf7add63972c
V11Playko <heinnervega20@gmail.com>
2022-08-01 21:16:16 -0500

    Bucles while y do while

--- Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Main.kt
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Main.kt
@@ -2,45 +2,16 @@
 const val PI = 3.1416
 
 fun main(args: Array<String>) {
-    val pais = "Argentina"
-
-    when(pais) {
-        "Argentina" -> {
-            println("El idioma de ${pais} es Español");
-        }
-
-        "Alemania" -> {
-            println("El idioma de ${pais} es Aleman");
-        }
-
-        "EEUU" -> {
-            println("El idioma de ${pais} es Ingles");
-        }
-
-        "Brasil" -> {
-            println("El idioma de ${pais} es Portugues");
-        }
-
-        else -> {
-            println("No conocemos el idioma");
-        }
+    var contador = 10
+    while (contador > 0){
+        println("El valor del contador es ${contador}")
+        contador--
     }
 
-    val edad = 33;
+    do {
+        println("Generando numero aleatorio")
+        val numeroAleaorio = (0..100).random()
+        println("EL numero generado es ${numeroAleaorio}")
+    }while (numeroAleaorio > 50) 
 
-    when(edad) {
-        in 0..3 -> {
-            println("Tu edad es ${edad} eres un bebe");
-        } in 4..11 -> {
-        println("Tu edad es ${edad} eres un niño");
-    } in 12..18 -> {
-        println("Tu edad es ${edad} eres un adolecente");
-    } in 19..59 -> {
-        println("Tu edad es ${edad} eres un adulto");
-    } in 60..99 -> {
-        println("Tu edad es ${edad} eres un adulto mayor");
-    } else -> {
-        println("😲");
-    }
-    }
 }
