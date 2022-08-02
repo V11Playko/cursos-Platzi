592ad02c5d23b6187f4e956fc1e6e750e9dbd22a
V11Playko <heinnervega20@gmail.com>
2022-08-02 08:10:51 -0500

    ordenando listas con las funciones que tiene Kotlin

--- Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Listas.kt
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/Listas.kt
@@ -38,7 +38,43 @@ fun main(args: Array<String>) {
     println("Nuestro array $myArray")
     println("Array como lista ${myArray.toList()}")
 
+    println("..")
+    println("..")
+    println("..")
 
 
+    val numerosDeLoteria = listOf(11,22,43,56,78,66)
+
+    // Ordenar con Sort
+    val numerosSorted = numerosDeLoteria.sorted()
+    println(numerosDeLoteria)
+
+    // Ordenar por orden descendiente
+    val numerosDeLoteriaDescendiente = numerosDeLoteria.sortedDescending()
+    println(numerosDeLoteriaDescendiente)
+
+    // Ordenar por condición
+    val ordenarPorMultiplos = numerosDeLoteria.sortedBy { numero -> numero < 50 }
+    println(ordenarPorMultiplos)
+
+    // Shuffled
+
+    val numerosAleatorios = numerosDeLoteria.shuffled()
+    println(numerosAleatorios)
+
+    // Reversa
+
+    val numerosEnReversa = numerosDeLoteria.reversed()
+    println(numerosEnReversa)
+
+    // Convertir lista de un tipo en listas de otro tipo
+
+    val mensajesDeNumeros = numerosDeLoteria.map { numero -> "Tu numero de loteria es $numero" }
+    println(mensajesDeNumeros)
+
+    // Filtrar numeros
+
+    val numerosFiltrados = numerosDeLoteria.filter { numero -> numero > 50 }
+    println(numerosFiltrados)
 
 }
