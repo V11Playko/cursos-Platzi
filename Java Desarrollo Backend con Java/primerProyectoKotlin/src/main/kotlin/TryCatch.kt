b93a0e1e7013a5d20616930a30a7daafd6266d5b
V11Playko <heinnervega20@gmail.com>
2022-08-02 07:25:40 -0500

    Elvis operator

--- Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/TryCatch.kt
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/TryCatch.kt
@@ -1,23 +1,13 @@
 
 fun main(args: Array<String>) {
 
-    // Como lanzar una excepción
-    // throw IllegalStateException("Error!")
+    println("Practicando operador Elvis")
+    val m: String? = null
+    val l: Int = if (m != null) m.length else -1
+    println(l)
 
-
-    // Controlando las excepciones
-    try {
-        throw IllegalStateException("Error!")
-    } catch (e: Exception){
-        println("Ha ocurrido un error")
-    } finally{
-        println("Ejecutando código final...")
-    }
-
-    // Try como expresion
-    val primerValor = 10
-    val segundoValor = 0
-    val resultado: Int = try { primerValor / segundoValor } catch ( e: Exception ) { 0 }
-    println(resultado)
-
+    //Con el operador Elvis expresamos
+    // lo mismo que en la línea anterior
+    val c = m?.length ?: -1
+    println(c)
 }
