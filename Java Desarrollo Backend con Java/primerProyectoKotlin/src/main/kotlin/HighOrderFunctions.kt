ecf41c562c263b1c6f7c7596817f109049c397fa
V11Playko <heinnervega20@gmail.com>
2022-08-03 07:44:52 -0500

    High Order functions

---
+++ Java Desarrollo Backend con Java/primerProyectoKotlin/src/main/kotlin/HighOrderFunctions.kt
@@ -0,0 +1,41 @@
+
+fun main(args: Array<String>) {
+
+    // Pasar lambda como parámetro a una función de alto orden
+    val largoDelValorInicial = superFuncion(valorInicial = "Hola!", block = {
+            valorInicial -> valorInicial.length
+    })
+    println(largoDelValorInicial)
+
+    val largoDelValorInicial2 = superFuncion(valorInicial = "Hola!") { valorInicial -> valorInicial.length }
+    println(largoDelValorInicial2)
+
+    // Como obtener una lambda de una función
+    val obtenerLambda = funcionInception("Carlos")
+
+    // Como obtener el valor de una lambda obtenida como resultado de otra función
+    val valorDeLambda = obtenerLambda()
+    println(valorDeLambda)
+
+    // Como obtener el valor de una lambda declarada como variable
+
+    val lambdaComoVariable : (String) -> String = { mensaje -> "El mensaje es $mensaje" }
+    println(lambdaComoVariable)
+
+    val lambdaComoVariable2 = { mensaje : String -> "El mensaje es $mensaje" }
+    println(lambdaComoVariable2)
+
+    val valorDeLambdaComoVariable = lambdaComoVariable("La función ha sido llamada con exito")
+    println(valorDeLambdaComoVariable)
+
+}
+
+fun superFuncion(valorInicial: String, block : (String) -> Int) : Int {
+    return block(valorInicial)
+}
+
+// Como devolver una función
+
+fun funcionInception(nombre: String) : () -> String {
+    return { "Hola desde la lambda $nombre" }
+}
