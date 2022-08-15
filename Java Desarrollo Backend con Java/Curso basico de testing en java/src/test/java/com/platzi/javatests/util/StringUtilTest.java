0345415091e22abf56fede7b01a82ebd5685e39d
V11Playko <heinnervega20@gmail.com>
2022-08-15 15:32:46 -0500

    C:\Users\Sebastian Vega\Documents\curso-testing-java-04-test-junit-passwords\src

--- Java Desarrollo Backend con Java/Curso basico de testing en java/src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ Java Desarrollo Backend con Java/Curso basico de testing en java/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,17 +1,27 @@
 package com.platzi.javatests.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
-
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
+    @Test
+    public void repeat_string_once() {
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 
-    private static void assertEquals(String actual, String expected) {
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
 
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to expected " + expected);
-        }
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
     }
 }
