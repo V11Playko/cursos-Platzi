c61ae54d37c210c4aabd3460771f097ccf8af362
V11Playko <heinnervega20@gmail.com>
2022-09-02 11:23:34 -0500

    Implementando la anotación @RestController

--- Java Desarrollo Backend con Java/Curso de Java Spring/src/main/java/com/demo/market/persistence/entity/Producto.java
+++ Java Desarrollo Backend con Java/Curso de Java Spring/src/main/java/com/demo/market/persistence/entity/Producto.java
@@ -87,4 +87,12 @@ public class Producto {
     public void setEstado(Boolean estado) {
         this.estado = estado;
     }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
