f9920ebcacc253f4264a7d04f3c2ce4fea7c191e
V11Playko <heinnervega20@gmail.com>
2022-09-01 19:28:27 -0500

    Mapear relaciones entre clases

---
+++ Java Desarrollo Backend con Java/Curso de Java Spring/src/main/java/com/demo/market/persistence/ProductoRepository.java
@@ -0,0 +1,23 @@
+package com.demo.market.persistence;
+
+import com.demo.market.persistence.crud.ProductoCrudRepository;
+import com.demo.market.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+}
