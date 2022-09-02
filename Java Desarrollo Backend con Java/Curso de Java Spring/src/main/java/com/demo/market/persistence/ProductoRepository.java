fee57d5569a96848dab1e0358288c386db5dc65f
V11Playko <heinnervega20@gmail.com>
2022-09-02 06:49:54 -0500

    Implementando la anotación @Repository

--- Java Desarrollo Backend con Java/Curso de Java Spring/src/main/java/com/demo/market/persistence/ProductoRepository.java
+++ Java Desarrollo Backend con Java/Curso de Java Spring/src/main/java/com/demo/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.demo.market.persistence;
 
 import com.demo.market.persistence.crud.ProductoCrudRepository;
 import com.demo.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
